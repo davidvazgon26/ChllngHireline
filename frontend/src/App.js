9f2e04a9a547f8f0c64c6c51a495235458d246d4
davidvazgon26 <davidvazgon@hotmail.com>
2022-09-25 03:53:16 -0500

    tabla para mostrar resultados y filtros en barra de busqueda

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+let url = 'http://images.amazon.com/images/P/0002005018.01.THUMBZZZ.jpg'
 
 function App() {
+
+  const[libros, setLibros]= useState([]);
+  const[tablaLibros, setTablaLibros] = useState([]);
+  const[busqueda, setBusqueda] = useState("");
+
+  const peticionGet = async() => {
+    await axios.get("http://localhost:3001/api/libros")
+    .then(response =>{
+      setLibros(Object.values(response.data)[0]);
+      setTablaLibros(Object.values(response.data)[0]);
+    }).catch(error=>console.log(error));
+  }
+
+  const handleChange=e=>{
+    setBusqueda(e.target.value);
+    filtrar(e.target.value);
+  }
+  
+  const filtrar=(terminoBusqueda)=>{
+    var resultadosBusqueda=tablaLibros.filter((elemento)=>{
+      if(elemento["Book\-Author"].toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      || elemento["Book\-Title"].toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      || elemento["Year\-Of\-Publication"].toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      || elemento.Publisher.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      ){
+        return elemento;
+      }
+    });
+    setLibros(resultadosBusqueda);
+  }
+
+
+
+
+  useEffect(()=>{
+    peticionGet();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+          {/* style={{ width:"90%"}} */}
+      <div className='containerInput'>
+        <input
+          className='form-control inputBuscar'
+          value={busqueda}
+          placeholder="Busqueda por Titulo, Autor, Año de publicacion o Editor "
+          onChange={handleChange}
+        />
+        <button className='btn btn-success'>
+          <FontAwesomeIcon icon={faSearch}/>
+        </button>
+      </div>
+
+      <div className='table-responsive'>
+      <table className='table table-sm table-bordered'>
+      <thead>
+        <tr>
+          <th>ISBN</th>
+          <th>Book Title</th>
+          <th>Book Author</th>
+          <th>Year of Publication</th>
+          <th>Publisher</th>
+          <th>Image</th>
+        </tr>
+      </thead>
+      {/* {console.log(libros)} */}
+      <tbody>
+        {
+          
+          libros&&
+          libros.map(libro=>(
+           
+            <tr key={libro._id}>
+              <td>{libro.ISBN}</td>
+              <td>{libro['Book\-Title']}</td>
+              <td>{libro['Book\-Author']}</td>
+              <td>{libro["Year\-Of\-Publication"]}</td>
+              <td>{libro.Publisher}</td>
+              <td><img src={libro["Image\-URL\-S"]} alt={libro["Image\-URL\-S"]}/></td>
+            </tr>
+          ))
+        }
+      </tbody>
+
+      </table>
+
+      </div>
+
+    
     </div>
   );
 }
